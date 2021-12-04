8723ca4db7425d52799c8046c037e698aa51b5ab
Samuelpj <samuel.pinheirojr@gmail.com>
2021-12-04 11:34:11 -0500

    First commit

---
+++ MetaWeather/MetaWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MetaWeather
+//
+//  Created by Samuel Pinheiro Junior on 04/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MetaWeather/MetaWeatherTests/MetaWeatherTests.swift
@@ -0,0 +1,33 @@
+//
+//  MetaWeatherTests.swift
+//  MetaWeatherTests
+//
+//  Created by Samuel Pinheiro Junior on 04/12/21.
+//
+
+import XCTest
+@testable import MetaWeather
+
+class MetaWeatherTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
